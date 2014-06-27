d313e5da84897e4adc7b4bd69f5407f7e4f33fca
reachingtech <15001130037@163.com>
2014-06-27 13:59:42 +0800

    Add new jquery mobile theme

---
+++ app/code/local/AHT/Aslideshow/Block/Adminhtml/Slideshow/Add/Form.php
@@ -0,0 +1,34 @@
+<?php
+<?php
+/**
+ * AHT_Aslideshow Extension
+ *
+ * @category    Local
+ * @package     AHT_Aslideshow
+ * @author      dungnv (dungnv@arrowhitech.com)
+ * @copyright   Copyright(c) 2011 Arrowhitech Inc. (http://www.arrowhitech.com)
+ *
+ */
+
+/**
+ *
+ * @category   Local
+ * @package    AHT_Aslideshow
+ * @author     dungnv <dungnv@arrowhitech.com>
+ */
+ 
+class AHT_Aslideshow_Block_Adminhtml_Slideshow_Add_Form extends Mage_Adminhtml_Block_Widget_Form {
+    protected function _prepareForm() {
+        $form = new Varien_Data_Form(
+                array(
+                        'id' => 'edit_form',
+                        'action' => $this->getUrl('*/*/save'),
+                        'method' => 'post',
+                )
+        );
+
+        $form->setUseContainer(true);
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
+}
